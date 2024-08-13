2f98f4625a2d5e6b3e61470bcee36cbb68cd0e9f
Владимир Шевченко <tiigrus@gmail.com>
2024-08-13 16:47:10 +0300

    feat: finished block stages

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // const slider = document.querySelector('.slider-js');
+    // const sliderList = slider.querySelector('.slider__list');
+    // const sliderItems = slider.querySelectorAll('.slider__item');
+
+    // let currentIndex = 0;
+
+    // function switchSlide() {
+    //     sliderItems[currentIndex].classList.remove('slider__item_active');
+    //     // currentIndex = (currentIndex + 1) % sliderItems.length;
+    //     if (currentIndex === sliderItems.length - 1) {
+    //         currentIndex = 1;
+    //         sliderList.style.transform = 'translateX(100)';
+    //     } else {
+    //         currentIndex++;
+    //         sliderList.style.transform = `translateX(-${currentIndex * 100}%)`;
+    //     }
+    //     sliderItems[currentIndex].classList.add('slider__item_active');
+    //     console.log(currentIndex)
+    // }
+
+    // setInterval(switchSlide, 2000);
+});
